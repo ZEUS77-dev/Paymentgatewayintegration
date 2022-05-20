8edecb92d78e9313bbba288248e8a58cb6f7ea85
Devadutta Hati <devhati83@gmail.com>
2022-05-20 06:58:09 +0530

    Initial commit

---
+++ main.js
@@ -0,0 +1,12 @@
+const borgir = document.querySelector(".borgir");
+const primnav = document.querySelector(".primary-navigation");
+borgir.addEventListener("click", function () {
+  borgir.classList.toggle("is-active");
+  primnav.classList.toggle("activate");
+});
+document.querySelectorAll(".primary-navigation a").forEach((n) =>
+  n.addEventListener("click", () => {
+    borgir.classList.remove("is-active");
+    primnav.classList.remove("activated");
+  })
+);
